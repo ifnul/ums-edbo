80b05b20dc403252d36321849f82b969cde57360
Ivan Ursul <ivan.ursul@optigra-soft.com>
2015-07-06 18:57:09 +0300

    Added Service Config

--- ums-edbo-rest-api/src/main/java/org/lnu/ums/Application.java
+++ ums-edbo-rest-api/src/main/java/org/lnu/ums/Application.java
@@ -1,15 +1,26 @@
 package org.lnu.ums;
 
+import org.lnu.ums.config.ControllerConfig;
+import org.lnu.ums.config.DaoConfig;
+import org.lnu.ums.facade.config.FacadeConfig;
+import org.lnu.ums.service.config.ServiceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Created by ivanursul on 6/17/15.
  */
 @SpringBootApplication
 @Configuration
+@Import({
+        ControllerConfig.class,
+        FacadeConfig.class,
+        ServiceConfig.class,
+        DaoConfig.class
+})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
---
+++ ums-edbo-service/src/main/java/org/lnu/ums/service/config/ServiceConfig.java
@@ -0,0 +1,12 @@
+package org.lnu.ums.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({
+        "org.lnu.ums.service"
+})
+public class ServiceConfig {
+
+}
