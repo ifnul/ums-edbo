2e29dc6ef725e0625a2310c284979f5a87efda9a
Ivan Ursul <Ivanon2@gmail.com>
2016-01-25 20:32:40 +0200

    Added guides controller

---
+++ ums-edbo-rest-api/src/main/java/org/lnu/ums/rest/controller/GuidesController.java
@@ -0,0 +1,11 @@
+package org.lnu.ums.rest.controller;
+
+import org.lnu.ums.rest.model.Constants;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(Constants.API_URL + "/persons")
+public class GuidesController extends BaseController {
+    
+}
