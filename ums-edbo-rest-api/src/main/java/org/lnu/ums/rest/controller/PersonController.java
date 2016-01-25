9816c91e7163b9e24878b43a47d2ea7e258bf327
Ivan Ursul <Ivanon2@gmail.com>
2016-01-25 20:28:44 +0200

    Added all API's from person wsdl

--- ums-edbo-rest-api/src/main/java/org/lnu/ums/rest/controller/PersonController.java
+++ ums-edbo-rest-api/src/main/java/org/lnu/ums/rest/controller/PersonController.java
@@ -7,10 +7,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ua.edboservice.ArrayOfDPersonAddresses2;
 import ua.edboservice.ArrayOfDPersonBenefits;
 import ua.edboservice.ArrayOfDPersonDocumentTypes;
+import ua.edboservice.ArrayOfDPersonDocuments;
+import ua.edboservice.ArrayOfDPersonDocumentsSubjects;
+import ua.edboservice.ArrayOfDPersonOlympiadsAwards;
 import ua.edboservice.ArrayOfDPersonRequestBenefits;
 import ua.edboservice.ArrayOfDPersonRequestDocumentSubjects;
+import ua.edboservice.ArrayOfDPersonRequestExaminations;
 import ua.edboservice.ArrayOfDPersonRequestOlympiadsAwards;
 import ua.edboservice.ArrayOfDPersonRequestQuotas;
 import ua.edboservice.ArrayOfDPersonsFind;
@@ -26,18 +31,34 @@ public class PersonController extends BaseController {
     private PersonService personService;
 
     @RequestMapping(value = "/requests/benefits")
-    public ArrayOfDPersonRequestBenefits getBenefits(@RequestParam("sessionGUID") String sessionGUID,
-                                                             @RequestParam("actualDate") String actualDate,
-                                                             @RequestParam("idLanguage") int languageId,
-                                                             @RequestParam("idPersonRequest") int personId) {
+    public ArrayOfDPersonRequestBenefits getBenefits(@RequestParam("SessionGUID") String sessionGUID,
+                                                             @RequestParam("ActualDate") String actualDate,
+                                                             @RequestParam("Id_Language") int languageId,
+                                                             @RequestParam("Id_PersonRequest") int personId) {
         LOGGER.info("Getting person Benefits, session Guid: {}, actualDate: {}, languageId: {}, personId: {}", sessionGUID, actualDate, languageId, personId);
         return personService.getRequestBenefits(sessionGUID, actualDate, languageId, personId);
     }
 
+    @RequestMapping(value = "/requests/documents/subjects")
+    public ArrayOfDPersonRequestDocumentSubjects personRequestDocumentSubjects(@RequestParam("SessionGUID") String sessionGUID,
+                                                                               @RequestParam("ActualDate") String actualDate,
+                                                                               @RequestParam("Id_Language") int languageId,
+                                                                               @RequestParam("Id_PersonRequest") int personId) {
+        return personService.getPersonRequestDocumentSubjects(sessionGUID, actualDate, languageId, personId);
+    }
+
+    @RequestMapping(value = "/requests/olympiads/awards")
+    public ArrayOfDPersonRequestOlympiadsAwards personRequestOlympiadAwards(@RequestParam("SessionGUID") String sessionGUID,
+                                                                            @RequestParam("ActualDate") String actualDate,
+                                                                            @RequestParam("Id_Language") int languageId,
+                                                                            @RequestParam("Id_PersonRequest") int personId) {
+        return personService.getRequestOlympiadAwards(sessionGUID, actualDate, languageId, personId);
+    }
+
     @RequestMapping
-    public ArrayOfDPersonsFind getPersons(@RequestParam("sessionGUID") String sessionGUID,
-                                       @RequestParam("actualDate") String actualDate,
-                                       @RequestParam("idLanguage") int idLanguage,
+    public ArrayOfDPersonsFind getPersons(@RequestParam("SessionGUID") String sessionGUID,
+                                       @RequestParam("ActualDate") String actualDate,
+                                       @RequestParam("Id_Language") int idLanguage,
                                        @RequestParam("fioMask") String fioMask,
                                        @RequestParam("documentSeries") String documentSeries,
                                        @RequestParam("documentNumber") String documentNumber,
@@ -51,42 +72,76 @@ public class PersonController extends BaseController {
     }
 
     @RequestMapping(value = "/documents/types")
-    public ArrayOfDPersonDocumentTypes personRequestDocumentSubjects(@RequestParam("sessionGUID") String sessionGUID,
-                                                                               @RequestParam("actualDate") String actualDate,
-                                                                               @RequestParam("idLanguage") int languageId) {
+    public ArrayOfDPersonDocumentTypes personRequestDocumentTypes(@RequestParam("SessionGUID") String sessionGUID,
+                                                                               @RequestParam("ActualDate") String actualDate,
+                                                                               @RequestParam("Id_Language") int languageId) {
         return personService.getDocumentTypes(sessionGUID, actualDate, languageId);
     }
 
-    @RequestMapping(value = "/documents/subjects")
-    public ArrayOfDPersonRequestDocumentSubjects personDocumentSubjects(@RequestParam("sessionGUID") String sessionGUID,
-                                                                        @RequestParam("actualDate") String actualDate,
-                                                                        @RequestParam("idLanguage") int languageId,
-                                                                        @RequestParam("idPersonRequest") int personId) {
-        return personService.getPersonDocumentSubjects(sessionGUID, actualDate, languageId, personId);
-    }
-
-    @RequestMapping(value = "/olympiads/awards")
-    public ArrayOfDPersonRequestOlympiadsAwards personOlympiadAwards(@RequestParam("sessionGUID") String sessionGUID,
-                                                                     @RequestParam("actualDate") String actualDate,
-                                                                     @RequestParam("idLanguage") int languageId,
-                                                                     @RequestParam("idPersonRequest") int personId) {
-        return personService.getOlympiadAwards(sessionGUID, actualDate, languageId, personId);
-    }
-
     @RequestMapping(value = "/benefits")
-    public ArrayOfDPersonBenefits getPersonBenefits(@RequestParam("sessionGUID") String sessionGUID,
-                                                    @RequestParam("actualDate") String actualDate,
-                                                    @RequestParam("idLanguage") int languageId,
-                                                    @RequestParam("idPersonRequest") int personId){
+    public ArrayOfDPersonBenefits getPersonBenefits(@RequestParam("SessionGUID") String sessionGUID,
+                                                    @RequestParam("ActualDate") String actualDate,
+                                                    @RequestParam("Id_Language") int languageId,
+                                                    @RequestParam("Id_PersonRequest") int personId){
         return personService.getBenefits(sessionGUID, actualDate, languageId, personId);
     }
 
     @RequestMapping(value = "/quotas")
-    public ArrayOfDPersonRequestQuotas getPersonQuotas(@RequestParam("sessionGUID") String sessionGUID,
-                                                    @RequestParam("actualDate") String actualDate,
-                                                    @RequestParam("idLanguage") int languageId,
-                                                    @RequestParam("idPersonRequest") int personId) {
+    public ArrayOfDPersonRequestQuotas getPersonQuotas(@RequestParam("SessionGUID") String sessionGUID,
+                                                    @RequestParam("ActualDate") String actualDate,
+                                                    @RequestParam("Id_Language") int languageId,
+                                                    @RequestParam("Id_PersonRequest") int personId) {
         return personService.getPersonQuotas(sessionGUID, actualDate, languageId, personId);
     }
 
+
+    @RequestMapping(value = "/documents/subjects")
+    public ArrayOfDPersonDocumentsSubjects personRequestDocumentSubjects(@RequestParam("SessionGUID") String sessionGUID,
+                                                                         @RequestParam("ActualDate") String actualDate,
+                                                                         @RequestParam("Id_Language") int languageId,
+                                                                         @RequestParam("Id_PersonRequest") int personId,
+                                                                         @RequestParam("idPersonDocument") int personDocumentId,
+                                                                         @RequestParam("Id_PersonDocumentType") int personDocumentTypeId
+                                                                         ) {
+        return personService.getPersonDocumentSubjects(sessionGUID, actualDate, languageId, personDocumentId, personId, personDocumentTypeId);
+    }
+
+    @RequestMapping(value = "/addresses2")
+    public ArrayOfDPersonAddresses2 getPersonsAdresses2(@RequestParam("SessionGUID") String sessionGUID,
+                                                        @RequestParam("ActualDate") String actualDate,
+                                                        @RequestParam("Id_Language") int languageId,
+                                                        @RequestParam("PersonCodeU") String personCodeU,
+                                                        @RequestParam("Id_PersonAddress") int personAddressId) {
+        return personService.getPersonAddresses2(sessionGUID, actualDate, languageId, personCodeU, personAddressId);
+    }
+
+    @RequestMapping(value = "/documents")
+    public ArrayOfDPersonDocuments getPersonDocuments(@RequestParam(value = "SessionGUID") String sessionGUID,
+                                                      @RequestParam(value = "ActualDate") String actualDate,
+                                                      @RequestParam(value = "Id_Language") int idLanguage,
+                                                      @RequestParam(value = "PersonCodeU") String personCodeU,
+                                                      @RequestParam(value = "Id_PersonDocumentType") int idPersonDocumentType,
+                                                      @RequestParam(value = "Id_PersonDocument") int idPersonDocument,
+                                                      @RequestParam(value = "UniversityKode") String universityKode,
+                                                      @RequestParam(value = "IsEntrantDocument") int isEntrantDocument) {
+        return personService.getPersonDocuments(sessionGUID, actualDate, idLanguage, personCodeU, idPersonDocumentType, idPersonDocument, universityKode, isEntrantDocument);
+    }
+
+    @RequestMapping(value = "/examinations")
+    public ArrayOfDPersonRequestExaminations getPersonExaminations(@RequestParam("SessionGUID") String sessionGUID,
+                                                                   @RequestParam("ActualDate") String actualDate,
+                                                                   @RequestParam("Id_Language") int languageId,
+                                                                   @RequestParam("Id_PersonRequest") int personId) {
+        return personService.getPersonExaminations(sessionGUID, actualDate, languageId, personId);
+    }
+
+
+    @RequestMapping(value = "/olympiads/awards")
+    public ArrayOfDPersonOlympiadsAwards persontOlympiadAwards(@RequestParam(value ="SessionGUID") String sessionGUID,
+                                                               @RequestParam(value ="ActualDate") String actualDate,
+                                                               @RequestParam(value ="Id_Language") int idLanguage,
+                                                               @RequestParam(value ="PersonCodeU") String personCodeU,
+                                                               @RequestParam(value ="Id_PersonRequestSeasons") int idPersonRequestSeasons) {
+        return personService.getOlympiadAwards(sessionGUID, actualDate, idLanguage, personCodeU, idPersonRequestSeasons);
+    }
 }
--- ums-edbo-service/src/main/java/org/lnu/ums/service/person/PersonService.java
+++ ums-edbo-service/src/main/java/org/lnu/ums/service/person/PersonService.java
@@ -1,10 +1,15 @@
 package org.lnu.ums.service.person;
 
 import org.lnu.ums.service.BaseEdboService;
+import ua.edboservice.ArrayOfDPersonAddresses2;
 import ua.edboservice.ArrayOfDPersonBenefits;
 import ua.edboservice.ArrayOfDPersonDocumentTypes;
+import ua.edboservice.ArrayOfDPersonDocuments;
+import ua.edboservice.ArrayOfDPersonDocumentsSubjects;
+import ua.edboservice.ArrayOfDPersonOlympiadsAwards;
 import ua.edboservice.ArrayOfDPersonRequestBenefits;
 import ua.edboservice.ArrayOfDPersonRequestDocumentSubjects;
+import ua.edboservice.ArrayOfDPersonRequestExaminations;
 import ua.edboservice.ArrayOfDPersonRequestOlympiadsAwards;
 import ua.edboservice.ArrayOfDPersonRequestQuotas;
 import ua.edboservice.ArrayOfDPersonsFind;
@@ -43,12 +48,12 @@ public class PersonService extends BaseEdboService<EDBOPersonSoap> {
         return handleResponse(response, sessionGUID);
     }
 
-    public ArrayOfDPersonRequestDocumentSubjects getPersonDocumentSubjects(String sessionGUID, String actualDate, int languageId, int personId) {
+    public ArrayOfDPersonRequestDocumentSubjects getPersonRequestDocumentSubjects(String sessionGUID, String actualDate, int languageId, int personId) {
         ArrayOfDPersonRequestDocumentSubjects response = client().personRequestDocumentSubjectsGet(sessionGUID, actualDate, languageId, personId);
         return handleResponse(response, sessionGUID);
     }
 
-    public ArrayOfDPersonRequestOlympiadsAwards getOlympiadAwards(String sessionGUID, String actualDate, int languageId, int personId) {
+    public ArrayOfDPersonRequestOlympiadsAwards getRequestOlympiadAwards(String sessionGUID, String actualDate, int languageId, int personId) {
         ArrayOfDPersonRequestOlympiadsAwards response = client().personRequestOlympiadsAwardsGet(sessionGUID, actualDate, languageId, personId);
         return handleResponse(response, sessionGUID);
     }
@@ -60,6 +65,32 @@ public class PersonService extends BaseEdboService<EDBOPersonSoap> {
 
     public ArrayOfDPersonRequestQuotas getPersonQuotas(String sessionGUID, String actualDate, int languageId, int personId) {
         ArrayOfDPersonRequestQuotas response = client().personRequestQuotasGet(sessionGUID, actualDate, languageId, personId);
+        return handleResponse(response, sessionGUID);
+    }
+
+    public ArrayOfDPersonDocumentsSubjects getPersonDocumentSubjects(String sessionGUID, String actualDate, int languageId, int personDocumentId, int personId, int personDocumentTypeId) {
+        ArrayOfDPersonDocumentsSubjects response = client().personDocumentsSubjectsGet(sessionGUID, actualDate, languageId, personDocumentId, personId, personDocumentTypeId);
+        return handleResponse(response, sessionGUID);
+    }
+
+    public ArrayOfDPersonAddresses2 getPersonAddresses2(String sessionGUID, String actualDate, int languageId, String personCodeU, int personAddressId) {
+        ArrayOfDPersonAddresses2 response = client().personAddressesGet2(sessionGUID, actualDate, languageId, personCodeU, personAddressId);
+        return handleResponse(response, sessionGUID);
+    }
+
+    public ArrayOfDPersonDocuments getPersonDocuments(String sessionGUID, String actualDate, int idLanguage, String personCodeU, int idPersonDocumentType,
+                                                      int idPersonDocument, String universityKode, int isEntrantDocument) {
+        ArrayOfDPersonDocuments response = client().personDocumentsGet(sessionGUID, actualDate, idLanguage, personCodeU, idPersonDocumentType, idPersonDocument, universityKode, isEntrantDocument);
+        return handleResponse(response, sessionGUID);
+    }
+
+    public ArrayOfDPersonRequestExaminations getPersonExaminations(String sessionGUID, String actualDate, int languageId, int personId) {
+        ArrayOfDPersonRequestExaminations response = client().personRequestExaminationsGet(sessionGUID, actualDate, languageId, personId);
+        return handleResponse(response, sessionGUID);
+    }
+
+    public ArrayOfDPersonOlympiadsAwards getOlympiadAwards(String sessionGUID, String actualDate, int idLanguage, String personCodeU, int idPersonRequestSeasons) {
+        ArrayOfDPersonOlympiadsAwards response = client().personOlympiadsAwardsGet(sessionGUID, actualDate, idLanguage, personCodeU, idPersonRequestSeasons);
         return handleResponse(response, sessionGUID);
     }
 }
