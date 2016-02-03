fbaea0550b5d8a58e2c69b4209478364eaab3765
Ivan Ursul <Ivanon2@gmail.com>
2016-02-03 19:17:04 +0200

    Added GetUniversityFacultySpecialitiesQuotasCmd command

---
+++ ums-edbo-cal/src/main/java/org/lnu/is/edbo/cal/GetUniversityFacultySpecialitiesQuotasCmd.java
@@ -0,0 +1,57 @@
+package org.lnu.is.edbo.cal;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import org.springframework.web.client.RestTemplate;
+import ua.edboservice.ArrayOfDUniversityFacultetSpecialitiesQuotas;
+
+public class GetUniversityFacultySpecialitiesQuotasCmd extends AbstractGetEdboCommand<ArrayOfDUniversityFacultetSpecialitiesQuotas> {
+
+    private String sessionGUID;
+    private int idLanguage;
+    private String actualDate;
+    private String universitySpecialitiesKode;
+    private String idQuota;
+
+    public GetUniversityFacultySpecialitiesQuotasCmd(String baseUrl, RestTemplate restTemplate) {
+        super("GetUniversityFacultySpecialtiesCmd", baseUrl, ArrayOfDUniversityFacultetSpecialitiesQuotas.class, restTemplate);
+    }
+
+    @Override
+    protected Multimap<String, String> getParams() {
+        Multimap<String, String> params = ArrayListMultimap.create();
+
+        put(params, "SessionGUID", sessionGUID);
+        put(params, "Id_Language", String.valueOf(idLanguage));
+        put(params, "ActualDate", actualDate);
+        put(params, "UniversitySpecialitiesKode", universitySpecialitiesKode);
+        put(params, "Id_Quota", idQuota);
+
+        return params;
+    }
+
+    @Override
+    protected String getApiUri() {
+        return "/api/guides/faculties/specialties/quotas";
+    }
+
+    public void setActualDate(String actualDate) {
+        this.actualDate = actualDate;
+    }
+
+    public void setIdLanguage(int idLanguage) {
+        this.idLanguage = idLanguage;
+    }
+
+    public void setIdQuota(String idQuota) {
+        this.idQuota = idQuota;
+    }
+
+    public void setSessionGUID(String sessionGUID) {
+        this.sessionGUID = sessionGUID;
+    }
+
+    public void setUniversitySpecialitiesKode(String universitySpecialitiesKode) {
+        this.universitySpecialitiesKode = universitySpecialitiesKode;
+    }
+}
